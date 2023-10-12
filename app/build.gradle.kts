c76f90165ef591b2f24d35712794b6c97bd76a40
Chelsea <cinthavong@gmail.com>
2023-10-12 14:41:22 -0700

    Second Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,19 @@ plugins {
 android {
     namespace = "com.example.carbonsequestrationcalculator"
     compileSdk = 33
+}
+
+android {
+        buildFeatures {
+            viewBinding; true
+        }
+
+
 
     defaultConfig {
         applicationId = "com.example.carbonsequestrationcalculator"
         minSdk = 24
-        targetSdk = 33
+        //targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -33,7 +41,7 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-}
+
 
 dependencies {
 
@@ -44,4 +52,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-}
+}}
+
--- app/src/main/java/com/example/carbonsequestrationcalculator/MainActivity.kt
+++ app/src/main/java/com/example/carbonsequestrationcalculator/MainActivity.kt
@@ -2,33 +2,66 @@ package com.example.carbonsequestrationcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.carbonsequestrationcalculator.R.id.calculateButton
+import kotlin.math.pow
+import kotlin.math.sqrt
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity(), Parcelable {
+    constructor(parcel: Parcel) : this() {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val button = findViewById<TextView>(R.id.button
-//        button.setOnClickListener { it: View!
-//            showResult()
-    //     )
+        val calculateButton = findViewById<TextView>(R.id.calculateButton)
+        calculateButton.setOnClickListener {
+            showResult()
         }
-
-//    private fun calculateResult(): Double {
-//        val canopyheightText = findViewById<EditText>(R.id.canopyHeight)
-//        val canopywidthText = findViewById<EditText>(R.id.canopyWidth)
-//
-//        val canopyHeight = canopyHeightText.text.toString().toDouble()
-//        val canopyWidth = canopyWidthText.text.toString().toDouble()
-//
-//        val dbH: Double = dbH.sqrt((canopyHeight)^2 + (canopyWidth)^2))
     }
-//    private fun showResult() {
-//        val abgResult = calculateResult()
-//
-//        val resultText = findViewById<TextView>(R.id.result)
-//        val resultText = ("%.2f".format(abgResult))
-//
-//        abgText.text = resultText
-//    }
+
+    private fun calculateResult(): Double {
+        val heightText = findViewById<EditText>(R.id.canopyHeight)
+        val widthText = findViewById<EditText>(R.id.canopyWidth)
+
+        val canopyHeight = heightText.text.toString().toDouble()
+        val canopyWidth = widthText.text.toString().toDouble()
+
+        val agbResult: Double = canopyHeight.pow(canopyHeight) + canopyWidth.pow(canopyWidth)
+        return sqrt(agbResult)
+    }
+
+    private fun showResult() {
+        val agbResult = calculateResult()
+
+        val agbText: TextView = findViewById(R.id.agbResult)
+        val resultText: String = ("%.2f".format(agbResult))
+
+        agbText.text = resultText
+    }
+
+}
 
